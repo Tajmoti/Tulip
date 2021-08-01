81015f14e4438e32ff825fe4575a3d26db801c0e
Timotej Šulík <timotej.sulik@gmail.com>
2021-08-01 17:27:54 +0200

    Remove useless files

--- libtvprovider-primewire/src/main/java/com/tajmoti/libprimewiretvprovider/PrimewireStreamableId.kt
+++
@@ -1,8 +0,0 @@
-package com.tajmoti.libprimewiretvprovider
-
-import java.io.Serializable
-
-data class PrimewireStreamableId(
-    val name: String,
-    val streamPageUrl: String,
-) : Serializable
--- libtvprovider-primewire/src/main/java/com/tajmoti/libprimewiretvprovider/PrimewireTvShowId.kt
+++
@@ -1,8 +0,0 @@
-package com.tajmoti.libprimewiretvprovider
-
-import java.io.Serializable
-
-data class PrimewireTvShowId(
-    val name: String,
-    val url: String
-): Serializable
