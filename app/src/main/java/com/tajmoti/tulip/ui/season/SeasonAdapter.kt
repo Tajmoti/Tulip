c6aba5f4bace9fe566dc40c8b6a58be7d4f8b624
Timotej Šulík <timotej.sulik@gmail.com>
2021-08-08 01:15:26 +0200

    Search, episode and streams UI polishing

--- app/src/main/java/com/tajmoti/tulip/ui/season/SeasonAdapter.kt
+++ app/src/main/java/com/tajmoti/tulip/ui/season/SeasonAdapter.kt
@@ -2,15 +2,19 @@ package com.tajmoti.tulip.ui.season
 
 import com.tajmoti.libtvprovider.show.Episode
 import com.tajmoti.tulip.BaseAdapter
+import com.tajmoti.tulip.R
 import com.tajmoti.tulip.databinding.ItemEpisodeBinding
 
 class SeasonAdapter :
     BaseAdapter<Episode, ItemEpisodeBinding>(ItemEpisodeBinding::inflate) {
 
     override fun onBindViewHolder(vh: Holder<ItemEpisodeBinding>, item: Episode) {
-        var label = "Episode ${item.number}"
-        if (item.name != null)
-            label += ": ${item.name}"
-        vh.binding.textEpisodeTitle.text = label // TODO
+        val numberText = if (item.number != null) {
+            vh.itemView.context.getString(R.string.episode_number, item.number)
+        } else {
+            vh.itemView.context.getString(R.string.episode_special)
+        }
+        vh.binding.textEpisodeNumber.text = numberText
+        vh.binding.textEpisodeTitle.text = item.name ?: ""
     }
 }
