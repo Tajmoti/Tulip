Tajmoti/Tulip

commit 1bb6cd1e9a53749f0a5a5852b76dcab487c26c9d

Replace NestedScrollView with RecyclerView header item to fix TV show screen lag

app/src/main/kotlin/com/tajmoti/tulip/ui/show/TvShowFragment.kt
@@ -1,20 +1,21 @@
 package com.tajmoti.tulip.ui.show
 
-import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowInsets
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
-import androidx.appcompat.widget.Toolbar
+import androidx.core.view.updatePadding
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DividerItemDecoration
-import com.bumptech.glide.Glide
+import androidx.viewbinding.ViewBinding
 import com.tajmoti.libtulip.model.info.TulipEpisodeInfo
 import com.tajmoti.libtulip.model.info.TulipSeasonInfo
-import com.tajmoti.libtulip.model.key.EpisodeKey
 import com.tajmoti.libtulip.ui.tvshow.TvShowViewModel
 import com.tajmoti.tulip.databinding.ActivityTabbedTvShowBinding
+import com.tajmoti.tulip.databinding.LayoutTvShowHeaderBinding
 import com.tajmoti.tulip.ui.*
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -35,69 +36,58 @@ class TvShowFragment : BaseFragment<ActivityTabbedTvShowBinding>(
     private lateinit var episodesAdapter: EpisodesAdapter
 
     /**
-     * Toolbar included in the layout.
+     * TV show info header. Inflated when RecyclerView requests it.
      */
-    private lateinit var toolbar: Toolbar
+    private var header: LayoutTvShowHeaderBinding? = null
 
     /**
-     * Previously selected season (restore on re-creation)
+     * Padding to be applied to the toolbar included in the header item.
      */
-    private var selectedSeasonIndex: Int? = null
+    private var topPadding = 0
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        selectedSeasonIndex = savedInstanceState
-            ?.getInt(SAVED_STATE_SELECTED_SEASON, -1)
-            .takeIf { it != -1 }
 
         binding.viewModel = viewModel
-        setupHeader(view.context)
-        binding.recyclerTvShow.adapter = episodesAdapter
-        val divider = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
-        binding.recyclerTvShow.addItemDecoration(divider)
-        consume(viewModel.backdropPath) { it?.let { onBackdropPathChanged(it) } }
-        consume(viewModel.seasons) { it?.let { onSeasonsChanged(it) } }
-        consume(viewModel.name) { it?.let { onNameChanged(it) } }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val selectedSeason = binding.header.spinnerSelectSeason.selectedItemPosition
-        outState.putInt(SAVED_STATE_SELECTED_SEASON, selectedSeason)
-    }
-
-    private fun setupHeader(context: Context) {
-        val binding = binding.header
-        toolbar = binding.toolbar
-        binding.viewModel = viewModel
-        binding.fab.setOnClickListener { viewModel.toggleFavorites() }
-        seasonsAdapter = ArrayAdapter<String>(
-            context,
-            android.R.layout.simple_spinner_dropdown_item
-        )
+        seasonsAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item)
         episodesAdapter = EpisodesAdapter(
-            { goToStreamsScreen(it.key) },
-            { showEpisodeDetailsDialog(requireContext(), it) }
+            this::goToStreamsScreen,
+            this::showEpisodeDetailsDialog,
+            this::inflateAndSetupHeader
         )
+        binding.recyclerTvShow.setupWithAdapterAndDivider(episodesAdapter)
+        consume(viewModel.seasons, this::onSeasonsChanged)
+        consume(viewModel.selectedSeason, this::onSeasonSelected)
+
+        binding.recyclerTvShow.setOnApplyWindowInsetsListener { _, insets ->
+            topPadding = insets.getInsets(WindowInsets.Type.systemBars()).top
+            insets
+        }
+    }
+
+    private fun inflateAndSetupHeader(inflater: LayoutInflater, root: ViewGroup, attach: Boolean): ViewBinding {
+        val binding = LayoutTvShowHeaderBinding.inflate(inflater, root, attach)
+        binding.lifecycleOwner = viewLifecycleOwner
+        header = binding
+        binding.viewModel = viewModel
+        binding.toolbar.updatePadding(top = topPadding)
         binding.spinnerSelectSeason.adapter = seasonsAdapter
         binding.spinnerSelectSeason.onItemSelectedListener = SpinnerListener()
+        (requireActivity() as MainActivity).swapActionBar(binding.toolbar)
+        binding.toolbar.title = ""
+        viewModel.seasons.value?.let(this::onSeasonsChanged)
+        return binding
     }
 
     inner class SpinnerListener : OnItemSelectedListener {
-        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-            onSeasonClicked(p2)
+        override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            onSeasonClicked(position)
         }
 
-        override fun onNothingSelected(p0: AdapterView<*>?) {
+        override fun onNothingSelected(parent: AdapterView<*>?) {
 
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        (requireActivity() as MainActivity).swapActionBar(toolbar)
-        toolbar.title = ""
     }
 
     override fun onStop() {
@@ -105,38 +95,33 @@ class TvShowFragment : BaseFragment<ActivityTabbedTvShowBinding>(
         (requireActivity() as MainActivity).swapActionBar(null)
     }
 
-    private fun onNameChanged(name: String) {
-        binding.header.title.text = name
-    }
-
-    private fun onBackdropPathChanged(backdropPath: String) {
-        Glide.with(this).load(backdropPath).into(binding.header.imgTvShow)
-    }
-
-    private fun onSeasonsChanged(seasons: List<TulipSeasonInfo>) {
-        val oldPos = selectedSeasonIndex ?: binding.header.spinnerSelectSeason.selectedItemPosition
-        selectedSeasonIndex = null
+    private fun onSeasonsChanged(seasons: List<TulipSeasonInfo>?) {
+        seasons ?: return
+        val header = header ?: return
+        val oldPos = seasons.indexOf(viewModel.selectedSeason.value).takeIf { it != -1 }
+            ?: header.spinnerSelectSeason.selectedItemPosition
         seasonsAdapter.clear()
         seasonsAdapter.addAll(seasons.map { getSeasonTitle(requireContext(), it) })
         if (oldPos < seasons.size)
-            binding.header.spinnerSelectSeason.setSelection(oldPos)
+            header.spinnerSelectSeason.setSelection(oldPos)
     }
 
     private fun onSeasonClicked(index: Int) {
         val item = viewModel.seasons.value?.getOrNull(index) ?: return
-        onSeasonChanged(item.episodes)
+        viewModel.onSeasonSelected(item)
     }
 
-    private fun onSeasonChanged(episodes: List<TulipEpisodeInfo>) {
-        episodesAdapter.items = episodes
+    private fun onSeasonSelected(season: TulipSeasonInfo?) {
+        season ?: return
+        episodesAdapter.items = season.episodes
     }
 
-    private fun goToStreamsScreen(episodeKey: EpisodeKey) {
-        TvShowFragmentDirections.actionNavigationTvShowToVideoPlayerActivity(episodeKey)
+    private fun goToStreamsScreen(episode: TulipEpisodeInfo) {
+        TvShowFragmentDirections.actionNavigationTvShowToVideoPlayerActivity(episode.key)
             .let { findNavController().navigate(it) }
     }
 
-    companion object {
-        private const val SAVED_STATE_SELECTED_SEASON = "selected_season"
+    private fun showEpisodeDetailsDialog(episode: TulipEpisodeInfo) {
+        showEpisodeDetailsDialog(requireContext(), episode)
     }
 }

app/src/main/kotlin/com/tajmoti/tulip/ui/BaseAdapter.kt
@@ -12,8 +12,12 @@ abstract class BaseAdapter<T, B : ViewBinding>(
     /**
      * Callback to use for when the item is clicked.
      */
-    private val callback: ((T) -> Unit)? = null
-) : RecyclerView.Adapter<BaseAdapter.Holder<B>>() {
+    private val callback: ((T) -> Unit)? = null,
+    /**
+     * Optional header view.
+     */
+    private val headerCreator: ((LayoutInflater, ViewGroup, Boolean) -> ViewBinding)? = null
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     /**
      * Items to display in this adapter.
      * Diffs are calculated automatically.
@@ -55,20 +59,37 @@ abstract class BaseAdapter<T, B : ViewBinding>(
         super.onAttachedToRecyclerView(recyclerView)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder<B> {
+    override fun getItemViewType(position: Int): Int {
+        return if (headerCreator != null && position == 0) {
+            ITEM_TYPE_HEADER
+        } else {
+            ITEM_TYPE_ITEM
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder<*> {
         val inflater = LayoutInflater.from(parent.context)
+        if (viewType == ITEM_TYPE_HEADER) {
+            return Holder(headerCreator!!.invoke(inflater, parent, false))
+        }
         val binding = bindingCreator(inflater, parent, false)
         return Holder(binding)
     }
 
-    override fun onBindViewHolder(holder: Holder<B>, position: Int) {
-        val item = items[position]
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (headerCreator != null && position == 0) {
+            return
+        }
+        @Suppress("UNCHECKED_CAST")
+        holder as Holder<B>
+        val pos = adjustPosHeader(position)
+        val item = items[pos]
         callback?.let { cb -> holder.itemView.setOnClickListener { cb(item) } }
         onBindViewHolder(context, holder.adapterPosition, holder.binding, item)
     }
 
     override fun getItemCount(): Int {
-        return items.size
+        return adjustByHeader(items.size)
     }
 
     class Holder<B : ViewBinding>(val binding: B) : RecyclerView.ViewHolder(binding.root)
@@ -78,16 +99,49 @@ abstract class BaseAdapter<T, B : ViewBinding>(
         private var oldVideos: List<T>
     ) : DiffUtil.Callback() {
 
-        override fun getOldListSize() = oldVideos.size
+        override fun getOldListSize(): Int {
+            return adjustByHeader(oldVideos.size)
+        }
 
-        override fun getNewListSize() = newVideos.size
+        override fun getNewListSize(): Int {
+            return adjustByHeader(newVideos.size)
+        }
 
         override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return this@BaseAdapter.areItemsTheSame(oldVideos[oldItemPosition], newVideos[newItemPosition])
+            if (oldItemPosition == 0 || newItemPosition == 0) {
+                return oldItemPosition == 0 && newItemPosition == 0
+            }
+            val oldItemPos = adjustPosHeader(oldItemPosition)
+            val newItemPos = adjustPosHeader(newItemPosition)
+            return this@BaseAdapter.areItemsTheSame(oldVideos[oldItemPos], newVideos[newItemPos])
         }
 
         override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return this@BaseAdapter.areContentsTheSame(oldVideos[oldItemPosition], newVideos[newItemPosition])
+            if (oldItemPosition == 0 || newItemPosition == 0) {
+                return oldItemPosition == 0 && newItemPosition == 0
+            }
+            val oldItemPos = adjustPosHeader(oldItemPosition)
+            val newItemPos = adjustPosHeader(newItemPosition)
+            return this@BaseAdapter.areContentsTheSame(oldVideos[oldItemPos], newVideos[newItemPos])
         }
+    }
+
+    private fun adjustByHeader(size: Int): Int {
+        var count = size
+        if (headerCreator != null)
+            count++
+        return count
+    }
+
+    private fun adjustPosHeader(size: Int): Int {
+        var pos = size
+        if (headerCreator != null)
+            pos--
+        return pos
+    }
+
+    companion object {
+        private const val ITEM_TYPE_HEADER = 0
+        private const val ITEM_TYPE_ITEM = 1
     }
 }

app/src/main/kotlin/com/tajmoti/tulip/ui/DataBindingAdapters.kt
@@ -8,6 +8,7 @@ import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.annotation.ColorInt
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.progressindicator.CircularProgressIndicator
 
@@ -22,6 +23,12 @@ object DataBindingAdapters {
     @BindingAdapter("android:src")
     fun setImageResource(imageView: ImageView, resource: Int) {
         imageView.setImageResource(resource)
+    }
+
+    @JvmStatic
+    @BindingAdapter("srcUrl")
+    fun setImageResourceUrl(imageView: ImageView, srcUrl: String?) {
+        Glide.with(imageView).load(srcUrl).into(imageView)
     }
 
     @JvmStatic

app/src/main/kotlin/com/tajmoti/tulip/ui/show/EpisodesAdapter.kt
@@ -1,6 +1,10 @@
 package com.tajmoti.tulip.ui.show
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
 import com.tajmoti.libtulip.model.info.TulipEpisodeInfo
 import com.tajmoti.libtulip.ui.player.VideoPlayerUtils.episodeToLabel
 import com.tajmoti.tulip.R
@@ -10,8 +14,13 @@ import com.tajmoti.tulip.ui.loadImageAsBackground
 
 class EpisodesAdapter(
     val onPlayClickedListener: ((TulipEpisodeInfo) -> Unit),
-    onDetailsClickedListener: ((TulipEpisodeInfo) -> Unit)
-) : BaseAdapter<TulipEpisodeInfo, ItemEpisodeBinding>(ItemEpisodeBinding::inflate, onDetailsClickedListener) {
+    onDetailsClickedListener: ((TulipEpisodeInfo) -> Unit),
+    headerCreator: ((LayoutInflater, ViewGroup, Boolean) -> ViewBinding)? = null
+) : BaseAdapter<TulipEpisodeInfo, ItemEpisodeBinding>(
+    ItemEpisodeBinding::inflate,
+    onDetailsClickedListener,
+    headerCreator
+) {
 
     override fun onBindViewHolder(context: Context, index: Int, binding: ItemEpisodeBinding, item: TulipEpisodeInfo) {
         binding.episodeNumberAndTitle = episodeToLabel(item)

app/src/main/res/layout/activity_tabbed_tv_show.xml
@@ -14,28 +14,14 @@
         android:layout_width="match_parent"
         android:layout_height="match_parent">
 
-        <androidx.core.widget.NestedScrollView
+        <androidx.recyclerview.widget.RecyclerView
+            android:id="@+id/recycler_tv_show"
             android:layout_width="match_parent"
             android:layout_height="match_parent"
-            android:fillViewport="true">
-
-            <LinearLayout
-                android:layout_width="match_parent"
-                android:layout_height="match_parent"
-                android:orientation="vertical"
-                android:visibility="@{viewModel.seasons != null &amp;&amp; !viewModel.error}">
-
-                <include
-                    android:id="@+id/header"
-                    layout="@layout/layout_tv_show_header" />
-
-                <androidx.recyclerview.widget.RecyclerView
-                    android:id="@+id/recycler_tv_show"
-                    android:layout_width="match_parent"
-                    android:layout_height="match_parent"
-                    app:layoutManager="LinearLayoutManager" />
-            </LinearLayout>
-        </androidx.core.widget.NestedScrollView>
+            android:clipToPadding="false"
+            android:fitsSystemWindows="false"
+            android:visibility="@{viewModel.seasons != null &amp;&amp; !viewModel.error}"
+            app:layoutManager="LinearLayoutManager" />
 
         <LinearLayout
             android:layout_width="wrap_content"

app/src/main/res/layout/layout_tv_show_header.xml
@@ -19,6 +19,7 @@
             android:layout_width="match_parent"
             android:layout_height="match_parent"
             android:scaleType="centerCrop"
+            app:srcUrl="@{viewModel.backdropPath}"
             tools:ignore="ContentDescription" />
 
         <View
@@ -36,7 +37,6 @@
             android:id="@+id/toolbar"
             android:layout_width="match_parent"
             android:layout_height="wrap_content"
-            android:fitsSystemWindows="true"
             app:layout_constraintTop_toTopOf="parent"
             app:popupTheme="@style/Theme.Tulip.PopupOverlay"
             app:title="" />
@@ -51,6 +51,7 @@
             android:fontFamily="sans-serif-condensed-light"
             android:gravity="center_horizontal|bottom"
             android:maxLines="1"
+            android:text="@{viewModel.name}"
             android:textColor="@color/white"
             android:textStyle="bold"
             app:autoSizeMaxTextSize="48sp"
@@ -79,6 +80,7 @@
             android:drawableStart="@{viewModel.isFavorite() ? @drawable/ic_baseline_star_24 : @drawable/ic_baseline_star_border_24}"
             android:ems="15"
             android:lines="1"
+            android:onClick="@{() -> viewModel.toggleFavorites()}"
             android:text="@{viewModel.isFavorite() ? @string/library_remove : @string/library_add}"
             android:textColor="@color/design_default_color_on_primary"
             app:layout_constraintBottom_toBottomOf="parent"

libtulip/src/main/kotlin/com/tajmoti/libtulip/ui/tvshow/TvShowViewModel.kt
@@ -21,6 +21,11 @@ interface TvShowViewModel {
     val seasons: StateFlow<List<TulipSeasonInfo>?>
 
     /**
+     * Season to display episodes from.
+     */
+    val selectedSeason: StateFlow<TulipSeasonInfo?>
+
+    /**
      * True if an error occurred during show loading
      */
     val error: StateFlow<Boolean>
@@ -39,4 +44,9 @@ interface TvShowViewModel {
      * Adds or removes this show from favorites
      */
     fun toggleFavorites()
+
+    /**
+     * The user has selected a season.
+     */
+    fun onSeasonSelected(season: TulipSeasonInfo)
 }

libtulip/src/main/kotlin/com/tajmoti/libtulip/ui/tvshow/TvShowViewModelImpl.kt
@@ -34,6 +34,7 @@ class TvShowViewModelImpl constructor(
         .map(viewModelScope) { (it as? State.Success)?.backdropPath }
     override val seasons = state
         .map(viewModelScope) { (it as? State.Success)?.seasons?.let { s -> sortSpecialsLast(s) } }
+    override val selectedSeason = MutableStateFlow<TulipSeasonInfo?>(null)
     override val isFavorite = favoritesRepository.isFavorite(initialItemKey)
         .stateIn(viewModelScope, SharingStarted.Eagerly, false)
     override val error = state
@@ -60,6 +61,10 @@ class TvShowViewModelImpl constructor(
                 favoritesRepository.addUserFavorite(initialItemKey)
             }
         }
+    }
+
+    override fun onSeasonSelected(season: TulipSeasonInfo) {
+        selectedSeason.value = season
     }
 
     private fun fetchSeasonsToState(key: TvShowKey): Flow<StateWithName> {

commit b2fefff8a4e830df6b509f6cb31b7ae0999c04b4

Fix random crashes introduced in 1bb6cd1e

app/src/main/kotlin/com/tajmoti/tulip/ui/show/TvShowFragment.kt
@@ -114,7 +114,7 @@ class TvShowFragment : BaseFragment<ActivityTabbedTvShowBinding>(
     private fun onSelectedSeasonChanged(season: SeasonKey?) {
         val seasons = viewModel.seasons.value
         episodesAdapter.items = seasons
-            ?.first { it.key == season }
+            ?.firstOrNull { it.key == season }
             ?.episodes
             ?: return
         val spinner = header?.spinnerSelectSeason ?: return