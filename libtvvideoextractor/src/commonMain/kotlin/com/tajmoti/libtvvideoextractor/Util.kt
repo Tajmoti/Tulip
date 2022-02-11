9ba1c86b176ce8218aea34212b091ae87354e46d
Timotej Šulík <timotej.sulik@gmail.com>
2022-02-11 02:19:53 +0100

    fix: Workaround for a JS IR compiler bug

--- libtvvideoextractor/src/commonMain/kotlin/com/tajmoti/libtvvideoextractor/Util.kt
+++ libtvvideoextractor/src/commonMain/kotlin/com/tajmoti/libtvvideoextractor/Util.kt
@@ -5,17 +5,17 @@ package com.tajmoti.libtvvideoextractor
  * A function, which loads the provided URL into a real web browser and returns the page HTML source.
  * A WebDriver is used to load the full page, useless URLs are blocked with urlBlocker.
  */
-typealias WebDriverPageSourceLoader = suspend (url: String, urlBlocker: UrlBlocker) -> Result<String>
+interface WebDriverPageSourceLoader {
+    suspend fun load(url: String, urlBlocker: UrlBlocker): Result<String>
+}
 
 /**
  * A function, which loads the provided URL into a real web browser and returns the page HTML source.
  * A WebDriver is used to load the full page, useless URLs are blocked with urlBlocker.
  */
-typealias WebDriverPageSourceLoaderWithCustomJs = suspend (
-    url: String,
-    urlBlocker: UrlBlocker,
-    submitTriggerJsGenerator: (String) -> String
-) -> Result<String>
+interface WebDriverPageSourceLoaderWithCustomJs {
+    suspend fun load (url: String, urlBlocker: UrlBlocker, submitTriggerJsGenerator: (String) -> String): Result<String>
+}
 
 /**
  * A function, which loads the provided URL into a real web browser and returns the page HTML source.
