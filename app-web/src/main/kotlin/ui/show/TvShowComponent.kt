d1e6d91e621990c3578a66b19200d53ec63ecd22
Timotej Šulík <timotej.sulik@gmail.com>
2022-02-26 12:54:25 +0100

    feat(react): TV show UI - title and season dropdown

--- app-web/src/main/kotlin/ui/show/TvShowComponent.kt
+++ app-web/src/main/kotlin/ui/show/TvShowComponent.kt
@@ -2,12 +2,16 @@ package ui.show
 
 import com.tajmoti.libtulip.model.info.TulipEpisodeInfo
 import com.tajmoti.libtulip.model.info.TulipSeasonInfo
+import com.tajmoti.libtulip.model.info.seasonNumber
+import com.tajmoti.libtulip.model.key.SeasonKey
 import com.tajmoti.libtulip.ui.tvshow.TvShowViewModel
 import com.tajmoti.libtulip.ui.tvshow.TvShowViewModelImpl
+import org.w3c.dom.HTMLSelectElement
 import react.RBuilder
-import react.dom.div
-import react.dom.onClick
-import ui.*
+import react.dom.*
+import ui.ViewModelComponent
+import ui.listButton
+import ui.renderLoading
 
 class TvShowComponent(props: TvShowProps) : ViewModelComponent<TvShowProps, TvShowViewModel.State, TvShowViewModel>(props) {
     override fun getViewModel(): TvShowViewModel {
@@ -16,23 +20,26 @@ class TvShowComponent(props: TvShowProps) : ViewModelComponent<TvShowProps, TvSh
 
     override fun RBuilder.render() {
         val seasons = vmState.seasons
+        vmState.name?.let { name -> renderName(name) }
         if (seasons != null) {
-            renderSeasons(seasons)
+            val currentSeason = getCurrentSeason(seasons, vmState.selectedSeason)
+            renderSeasonDropdown(seasons, currentSeason)
+            renderSeason(currentSeason)
         } else {
             renderLoading()
         }
     }
 
-    private fun RBuilder.renderSeasons(it: List<TulipSeasonInfo>) {
-        div("list-group") {
-            for (season in it) {
-                renderSeason(season)
-            }
-        }
+    private fun RBuilder.renderName(name: String) {
+        h1("mb-4") { +name }
+    }
+
+    private fun getCurrentSeason(seasons: List<TulipSeasonInfo>, preselected: SeasonKey?): TulipSeasonInfo {
+        val currentSeasonKey = preselected ?: seasons.first().key
+        return seasons.first { it.key == currentSeasonKey }
     }
 
     private fun RBuilder.renderSeason(season: TulipSeasonInfo) {
-        activeListItem { +"Season ${season.key.seasonNumber}" }
         for (episode in season.episodes) {
             renderEpisode(episode)
         }
@@ -42,6 +49,31 @@ class TvShowComponent(props: TvShowProps) : ViewModelComponent<TvShowProps, TvSh
         listButton {
             +"${episode.episodeNumber} ${episode.name}"
             attrs.onClick = { _ -> props.onEpisodeClicked(episode.key) }
+        }
+    }
+
+    private fun RBuilder.renderSeasonDropdown(seasons: List<TulipSeasonInfo>, selected: TulipSeasonInfo) {
+        select("custom-select mb-2") {
+            for (season in seasons) {
+                renderSeasonDropdownItem(season)
+            }
+            attrs.value = selected.seasonNumber.toString()
+            attrs.onChange = {
+                val seasonNumber = (it.target as HTMLSelectElement).value.toInt()
+                onSeasonNumberSelected(seasons, seasonNumber)
+            }
+        }
+    }
+
+    private fun onSeasonNumberSelected(seasons: List<TulipSeasonInfo>, seasonNumber: Int) {
+        val season = seasons.first { season -> season.seasonNumber == seasonNumber }
+        viewModel.onSeasonSelected(season.key)
+    }
+
+    private fun RBuilder.renderSeasonDropdownItem(season: TulipSeasonInfo) {
+        option {
+            +"Season ${season.seasonNumber}"
+            attrs.value = season.seasonNumber.toString()
         }
     }
 }
