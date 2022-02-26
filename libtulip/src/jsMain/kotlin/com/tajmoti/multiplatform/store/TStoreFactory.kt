53b8dcd4ed6818ce65b295c57aa44891f259f8ed
Timotej Šulík <timotej.sulik@gmail.com>
2022-02-26 17:23:32 +0100

    feat(js): Use LocalStorage in TStoreFactory as a source of data

--- libtulip/src/jsMain/kotlin/com/tajmoti/multiplatform/store/TStoreFactory.kt
+++ libtulip/src/jsMain/kotlin/com/tajmoti/multiplatform/store/TStoreFactory.kt
@@ -2,10 +2,10 @@ package com.tajmoti.multiplatform.store
 
 import com.tajmoti.libtulip.TulipConfiguration
 import com.tajmoti.libtulip.misc.job.NetworkResult
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 
 actual object TStoreFactory {
+
     actual fun <Key : Any, Output : Any> createStore(
         cache: TulipConfiguration.CacheParameters,
         source: (Key) -> Flow<Result<Output>>,
@@ -14,7 +14,19 @@ actual object TStoreFactory {
     ): TStore<Key, Output> {
         return object : TStore<Key, Output> {
             override fun stream(key: Key): Flow<NetworkResult<Output>> {
-                return source(key).map(::toNetworkResult)
+                val fromMemory = reader
+                    ?.invoke(key)
+                    ?.map(::wrapNullable)
+                    ?: emptyFlow()
+                val fromNetwork = source(key)
+                    .map(::toNetworkResult)
+                    .onEach { result -> result.data?.let { value -> writer?.invoke(key, value) } }
+                return merge(fromNetwork, fromMemory).distinctUntilChanged()
+            }
+
+            private fun wrapNullable(maybeOutput: Output?): NetworkResult<Output> {
+                return maybeOutput?.let { output -> NetworkResult.Success(output) }
+                    ?: NetworkResult.Error(NetworkResult.ErrorType.CONVERSION_FAILED)
             }
         }
     }
