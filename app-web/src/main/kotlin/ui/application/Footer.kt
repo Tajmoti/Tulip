0d044b82da11ceab6027a8a2f183ed213301dd42
Timotej Šulík <timotej.sulik@gmail.com>
2022-03-05 00:48:55 +0100

    fix(react): Use router to navigate to "/" on "Reset" click

--- app-web/src/main/kotlin/ui/application/Footer.kt
+++ app-web/src/main/kotlin/ui/application/Footer.kt
@@ -1,6 +1,7 @@
 package ui.application
 
 import com.tajmoti.libtulip.TulipBuildInfo
+import kotlinx.browser.document
 import kotlinx.browser.localStorage
 import react.Props
 import react.dom.a
@@ -8,15 +9,21 @@ import react.dom.footer
 import react.dom.onClick
 import react.dom.span
 import react.fc
+import react.router.useNavigate
 
 val Footer = fc<Props> {
     footer("navbar navbar-dark bg-dark text-light mt-auto mt-2") {
+        val nav = useNavigate()
         span("navbar-text") {
             +"Build ${TulipBuildInfo.commit}"
         }
-        a("/", classes = "navbar-text") {
+        a("#", classes = "navbar-text") {
             +"Reset"
-            attrs.onClick = { localStorage.clear() }
+            attrs.onClick = {
+                localStorage.clear()
+                nav("/")
+                document.location?.reload()
+            }
         }
     }
 }
