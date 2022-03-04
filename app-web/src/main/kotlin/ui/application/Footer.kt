b07a3752db1140050bcd1c7e1b627744eaa3acc1
Timotej Šulík <timotej.sulik@gmail.com>
2022-03-05 00:26:58 +0100

    feat(react): Add "Reset" button to wipe local storage in case of breaking changes

--- app-web/src/main/kotlin/ui/application/Footer.kt
+++ app-web/src/main/kotlin/ui/application/Footer.kt
@@ -1,8 +1,11 @@
 package ui.application
 
 import com.tajmoti.libtulip.TulipBuildInfo
+import kotlinx.browser.localStorage
 import react.Props
+import react.dom.a
 import react.dom.footer
+import react.dom.onClick
 import react.dom.span
 import react.fc
 
@@ -10,6 +13,10 @@ val Footer = fc<Props> {
     footer("navbar navbar-dark bg-dark text-light mt-auto mt-2") {
         span("navbar-text") {
             +"Build ${TulipBuildInfo.commit}"
+        }
+        a("/", classes = "navbar-text") {
+            +"Reset"
+            attrs.onClick = { localStorage.clear() }
         }
     }
 }
