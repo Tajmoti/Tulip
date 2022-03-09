d991602268c1fb7425d1288b66db91a6efe8e299
Timotej Šulík <timotej.sulik@gmail.com>
2022-03-09 23:29:03 +0100

    fix(android): Fix runtime crash on Search
    Introduced in f6900a8b

--- commonutils/src/jvmMain/kotlin/com/tajmoti/commonutils/UrlEncoder.kt
+++ commonutils/src/jvmMain/kotlin/com/tajmoti/commonutils/UrlEncoder.kt
@@ -4,6 +4,6 @@ import java.net.URLEncoder
 
 actual object UrlEncoder {
     actual fun encode(url: String): String {
-        return URLEncoder.encode(url, Charsets.UTF_8)
+        return URLEncoder.encode(url, Charsets.UTF_8.name())
     }
 }

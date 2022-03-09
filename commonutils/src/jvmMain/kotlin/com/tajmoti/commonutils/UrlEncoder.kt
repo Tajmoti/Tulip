Tajmoti/Tulip

commit f6900a8b3bb2a4a0965341c2891af51b6e799d55

fix: Always use UTF-8 for URL encoding on the JVM

commonutils/src/jvmMain/kotlin/com/tajmoti/commonutils/UrlEncoder.kt
@@ -4,6 +4,6 @@ import java.net.URLEncoder
 
 actual object UrlEncoder {
     actual fun encode(url: String): String {
-        return URLEncoder.encode(url)
+        return URLEncoder.encode(url, Charsets.UTF_8)
     }
 }

commit d991602268c1fb7425d1288b66db91a6efe8e299

fix(android): Fix runtime crash on Search
Introduced in f6900a8b

commonutils/src/jvmMain/kotlin/com/tajmoti/commonutils/UrlEncoder.kt
@@ -4,6 +4,6 @@ import java.net.URLEncoder
 
 actual object UrlEncoder {
     actual fun encode(url: String): String {
-        return URLEncoder.encode(url, Charsets.UTF_8)
+        return URLEncoder.encode(url, Charsets.UTF_8.name())
     }
 }